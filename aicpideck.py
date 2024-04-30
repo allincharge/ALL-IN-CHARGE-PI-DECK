b488322f5cf0d3b5c7995358e394464ebdf0cad4
ALL IN CHARGE <167342500+allincharge@users.noreply.github.com>
2024-04-30 07:58:07 +0300

    Update aicpideck.py

--- aicpideck.py
+++ aicpideck.py
@@ -45,6 +45,20 @@ def check_vin(VINnumber):
     conn.close()
     return bool(result)
 
+def check_vin_get_plate(VINnumber):
+    """ Check if the provided VIN number exists in the database.
+    Returns the plate number if the VIN number exists, an empty string otherwise.
+    """
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute("SELECT plateNumber FROM vehicles WHERE VINnumber = ?", (VINnumber,))
+    result = c.fetchone()
+    conn.close()
+    if result:
+        return result[0]  # Return the plate number
+    else:
+        return ''  # Return an empty string
+
 @app.route('/add-data', methods=['POST'])
 def add_data():
     """
@@ -56,23 +70,23 @@ def add_data():
     plateNumber = data['plateNumber']
 
     if insert_data(VINnumber, plateNumber):
-        return jsonify({'status': True})
+        return jsonify({'status': True}), 202
     else:
         return jsonify({'status': False}), 500
     
 @app.route('/verify-vin', methods=['POST'])
 def verify_vin():
-    """
-    Endpoint to verify the provided VIN number only.
-    Returns a JSON response with the 'status' field set to True or False.
+    """ Endpoint to verify the provided VIN number.
+    Returns the plate number and status if the VIN exists in the database,
+    or a 'Not Found' message otherwise.
     """
     data = request.get_json()
     VINnumber = data['VINnumber']
-
-    if check_vin(VINnumber):
-        return jsonify({'status': True})
+    plate_number = check_vin_get_plate(VINnumber)
+    if plate_number:
+        return jsonify({'status': True, 'plateNumber': plate_number}), 202
     else:
-        return jsonify({'status': False})
+        return jsonify({'status': False, 'plateNumber': 'Not Found'}), 500
     
 @app.route('/sync-data', methods=['POST'])
 def receive_data():
